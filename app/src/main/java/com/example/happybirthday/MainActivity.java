650079188e7f2418c107eee8c13682336ddb05f3
Usaid-Dev <usaidasif12@gmail.com>
2020-07-01 21:16:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.happybirthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
